033e71fdcd105cd4e9166a7953005235f3b98b30
rishi-desai <rdesai82@gatech.edu>
2023-04-03 12:37:13 -0400

    possible fix for website reload when password is entered

--- index.js
+++ index.js
@@ -46,12 +46,18 @@ setRandomImage();
 function checkPassword(event) {
   event.preventDefault();
   const password = document.getElementById("password-input").value;
-  if (password === "decenter") {
+  if (password.toLowerCase().trim() == "decenter") {
     document.getElementById("password-form").classList.add("hidden");
     document.getElementById("secret-image").classList.add("show");
   } else {
     alert("Incorrect password. Please try again.");
   }
+}
+
+function submitForm(event) {
+  event.preventDefault(); // This will prevent the default form submission behavior
+
+  checkPassword(event);
 }
 
 // List of sentences
